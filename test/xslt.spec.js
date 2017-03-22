53f708ed299b5cbb4d3a81c7adcc9a2b11a6ae16
Martijn van de Rijdt <martijn@enketo.org>
2017-03-22 11:30:58 -0600

    changed: edited test description

--- test/xslt.spec.js
+++ test/xslt.spec.js
@@ -1,16 +1,16 @@
-var sheets = require("../index"),
-    assert = require("assert");
+var sheets = require('../index'),
+	assert = require('assert');
 
 describe('sheets', function() {
 
-    it('should return an xslForm sheet', function() {
-        assert.equal(typeof sheets.xslForm, 'string');
-        assert.equal(sheets.xslForm.length > 0, true);
-    });
+	it('should return an xslForm sheet', function() {
+		assert.equal(typeof sheets.xslForm, 'string');
+		assert.equal(sheets.xslForm.length > 0, true);
+	});
 
-    it('should return an xslForm sheet', function() {
-        assert.equal(typeof sheets.xslModel, 'string');
-        assert.equal(sheets.xslModel.length > 0, true);
-    });
+	it('should return an xslModel sheet', function() {
+		assert.equal(typeof sheets.xslModel, 'string');
+		assert.equal(sheets.xslModel.length > 0, true);
+	});
 
 });
